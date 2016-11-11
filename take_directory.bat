4aa8d2fb6473adcdfc99e50e2bd20302fa194879
lomassubedi <subedilomas@gmail.com>
2016-11-11 17:32:40 +0545

    changes made.

--- take_directory.bat
+++ take_directory.bat
@@ -1,7 +1,20 @@
 @echo off
 setlocal EnableDelayedExpansion
 
+rem Automate get git directory
+rem where /R C:\ sh.exe
+
 REM Git directory
+rem IF EXIST "%PROGRAMFILES(X86)%" (GOTO:64BIT) ELSE (GOTO:32BIT)
+rem 	:64BIT
+rem 	echo OS IS 64-BIT
+rem 	GOTO:END
+
+rem 	:32BIT
+rem 	echo OS IS 32-BIT
+rem 	GOTO:END
+rem :END
+
 set git_bash_dir="C:\Program Files\Git\bin\sh.exe"
 set script_file_name=\bash_script.sh
 
@@ -10,7 +23,7 @@ set dirVar=%cd%
 rem append the file name
 set final_path=%cd%%script_file_name%
 
-rem Get drive letter
+rem Get drive letter with colon
 set drive=%final_path:~0,2%
 echo %drive%
 
@@ -22,10 +35,3 @@ set unix_path=^"^/%unix_path%^"
 echo %unix_path%
 
 %git_bash_dir% --login -i -c %unix_path%
-
-rem set unix_path=%final_path% 
-rem set unix_path=%unix_path:\=/%
-rem set unix_path=%unix_path:C:=/c%
-rem set unix_path=^"%unix_path%^"
-rem echo %unix_path%
-rem echo %final_path%
---
+++ test_git_path.bat
@@ -0,0 +1,8 @@
+@echo off
+set git_path_1=C:\
+echo %git_path_1%
+IF EXIST git_path_1 (
+echo Yes 
+) ELSE (
+echo No
+)

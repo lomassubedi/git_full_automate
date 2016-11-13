0e4ce12f1dbb0184b0163cf8bc4f38f31c57ca03
lomassubedi <subedilomas@gmail.com>
2016-11-13 12:01:04 +0545

    changes made.

--- take_directory.bat
+++ take_directory.bat
@@ -27,15 +27,24 @@ rem Get drive letter with colon
 set drive=%final_path:~0,2%
 echo %drive%
 
+rem Copy path in DOS style 
 set unix_path=%final_path% 
+
+rem replace '\' with '/' for unix path conversion
 set unix_path=%unix_path:\=/%
+
+rem convert DOS drive notation to unix style drive i.e. C: to /C/
 set unix_path=!unix_path:%drive%=%drive:~0,1%!
+
+rem Add quotation marks at the end
 set unix_path=^"^/%unix_path%^" 
-set unix_path=%unix_path: =%
+
+rem discard white space at the end
+set unix_path=%unix_path: "="%
 
 rem set unix_path=%unix_path: =\ %
 
-set unix_path=%unix_path: =\ %
+rem set unix_path=%unix_path: =\ %
 echo %unix_path%
 
 %git_bash_dir% --login -i -c %unix_path%

285f1592f9df059841ed8e17a045c457f8cbf6a4
lomassubedi <subedilomas@gmail.com>
2016-11-13 11:56:18 +0545

    changes made.

--- take_directory.bat
+++ take_directory.bat
@@ -30,8 +30,12 @@ echo %drive%
 set unix_path=%final_path% 
 set unix_path=%unix_path:\=/%
 set unix_path=!unix_path:%drive%=%drive:~0,1%!
-
 set unix_path=^"^/%unix_path%^" 
+set unix_path=%unix_path: =%
+
+rem set unix_path=%unix_path: =\ %
+
+set unix_path=%unix_path: =\ %
 echo %unix_path%
 
 %git_bash_dir% --login -i -c %unix_path%
